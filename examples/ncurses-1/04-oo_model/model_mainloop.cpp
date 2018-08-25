0010fa7cd8a7304ce88f330738586e3002880f7a
Arthur Moraes do Lago <arthurmoraeslago@gmail.com>
2018-08-24 21:32:36 -0300

    Laboratorio 2 concluido

--- examples/ncurses-1/04-oo_model/model_mainloop.cpp
+++ examples/ncurses-1/04-oo_model/model_mainloop.cpp
@@ -1,3 +1,7 @@
+// Laboratorio 1
+// Arthur Moraes do Lago (157702)
+// Vitor Saldanha da Silveira (158508)
+
 #include <iostream>
 #include <chrono>
 #include <thread>
@@ -12,20 +16,24 @@ uint64_t get_now_ms() {
 
 int main ()
 {
-  Corpo *c1 = new Corpo(10, 0, 20);
-  Corpo *c2 = new Corpo(10, 0, 19);
-  Corpo *c3 = new Corpo(10, 0, 17);
-  Corpo *c4 = new Corpo(10, 0, 15);
+  Corpo *c1 = new Corpo(10, 0, 25, 20, 10, 5);
+  Corpo *c2 = new Corpo(10, 0, 20, 20, 10, 0);
+  Corpo *c3 = new Corpo(10, 0, 35, 20, 10, 2);
+  Corpo *c4 = new Corpo(10, 0, 10, 20, 10, 0);
+  Corpo *c5 = new Corpo(10, 0, 20, 0, 10, 0);
+  Corpo *c6 = new Corpo(10, 0, 0, 0, 10, 0);
 
   ListaDeCorpos *l = new ListaDeCorpos();
   l->add_corpo(c1);
   l->add_corpo(c2);
   l->add_corpo(c3);
   l->add_corpo(c4);
+  l->add_corpo(c5);
+  l->add_corpo(c6);
 
   Fisica *f = new Fisica(l);
 
-  Tela *tela = new Tela(l, 20, 20, 20, 20);
+  Tela *tela = new Tela(l, 20, 20);
   tela->init();
 
   uint64_t t0;
@@ -44,15 +52,15 @@ int main ()
     deltaT = t1-t0;
 
     // Atualiza modelo
-    f->update(deltaT);
+    f->update(deltaT, 0);
 
     // Atualiza tela
     tela->update();
 
     // Condicao de parada
-    if ( (t1-T) > 10000 ) break;
+    if ( (t1-T) > 30000 ) break;
 
-    std::this_thread::sleep_for (std::chrono::milliseconds(100));
+    std::this_thread::sleep_for (std::chrono::milliseconds(20));
     i++;
   }
   tela->stop();
--- examples/ncurses-1/04-oo_model/oo_model.cpp
+++ examples/ncurses-1/04-oo_model/oo_model.cpp
@@ -1,13 +1,20 @@
+// Laboratorio 1
+// Arthur Moraes do Lago (157702)
+// Vitor Saldanha da Silveira (158508)
+
 #include <vector>
 
 #include "oo_model.hpp"
 
 #include <ncurses.h>
 
-Corpo::Corpo(float massa, float velocidade, float posicao) {
+Corpo::Corpo(float massa, float velocidade, float posicao, float k_mola, float pos_equilibrio, float coef_atrito) {
   this->massa = massa;
   this->velocidade = velocidade;
   this->posicao = posicao;
+  this->k_mola = k_mola;
+  this->pos_equilibrio = pos_equilibrio;
+  this->coef_atrito = coef_atrito;
 }
 
 void Corpo::update(float nova_velocidade, float nova_posicao) {
@@ -26,24 +33,22 @@ float Corpo::get_velocidade() {
 float Corpo::get_posicao() {
   return this->posicao;
 }
+float Corpo::get_k_mola() {
+  return this->k_mola;
+}
+
+float Corpo::get_pos_equilibrio() {
+  return this->pos_equilibrio;
+}
+
+float Corpo::get_coef_atrito() {
+  return this->coef_atrito;
+}
+
 
 ListaDeCorpos::ListaDeCorpos() {
   this->corpos = new std::vector<Corpo *>(0);
 }
-
-void ListaDeCorpos::hard_copy(ListaDeCorpos *ldc) {
-  std::vector<Corpo *> *corpos = ldc->get_corpos();
-
-  for (int k=0; k<corpos->size(); k++) {
-    Corpo *c = new Corpo( (*corpos)[k]->get_massa(),\
-                          (*corpos)[k]->get_posicao(),\
-                          (*corpos)[k]->get_velocidade()
-                        );
-    this->add_corpo(c);
-  }
-
-}
-
 
 void ListaDeCorpos::add_corpo(Corpo *c) {
   (this->corpos)->push_back(c);
@@ -57,64 +62,58 @@ Fisica::Fisica(ListaDeCorpos *ldc) {
   this->lista = ldc;
 }
 
-void Fisica::update(float deltaT) {
+void Fisica::update(float deltaT, float entrada) {
   // Atualiza parametros dos corpos!
   std::vector<Corpo *> *c = this->lista->get_corpos();
   for (int i = 0; i < (*c).size(); i++) {
-    float new_vel = (*c)[i]->get_velocidade() + (float)deltaT * (-10.0)/1000;
+	// Adicionando mais forças ao sistema além da gravidade:
+	float forca = GRAVITY * (*c)[i]->get_massa() +
+			-(*c)[i]->get_k_mola() * ((*c)[i]->get_posicao() - (*c)[i]->get_pos_equilibrio()) +
+			-(*c)[i]->get_coef_atrito() * (*c)[i]->get_velocidade() +
+			entrada;
+    float new_vel = (*c)[i]->get_velocidade() + (float)deltaT *forca/(*c)[i]->get_massa()/1000;
     float new_pos = (*c)[i]->get_posicao() + (float)deltaT * new_vel/1000;
-    if (new_pos < 0) {
-      new_pos *= -1;
-      new_vel *= -1;
-    }
-
     (*c)[i]->update(new_vel, new_pos);
   }
 }
 
-Tela::Tela(ListaDeCorpos *ldc, int maxI, int maxJ, float maxX, float maxY) {
+
+Tela::Tela(ListaDeCorpos *ldc, float maxX, float maxY) {
   this->lista = ldc;
-  this->lista_anterior = new ListaDeCorpos();
-  this->lista_anterior->hard_copy(this->lista);
-  this->maxI = maxI;
-  this->maxJ = maxJ;
   this->maxX = maxX;
   this->maxY = maxY;
 }
 
 void Tela::init() {
   initscr();			       /* Start curses mode 		*/
-	raw();				         /* Line buffering disabled	*/
+  raw();				         /* Line buffering disabled	*/
   curs_set(0);           /* Do not display cursor */
+  getmaxyx(stdscr, this->maxI, this->maxJ);
 }
 
 void Tela::update() {
+
   int i;
 
-  std::vector<Corpo *> *corpos_old = this->lista_anterior->get_corpos();
-
   // Apaga corpos na tela
-  for (int k=0; k<corpos_old->size(); k++)
-  {
-    i = (int) ((*corpos_old)[k]->get_posicao()) * \
-        (this->maxI / this->maxX);
-    move(i, k);   /* Move cursor to position */
-    echochar(' ');  /* Prints character, advances a position */
+  for (i = 0; i < listaPontos.size(); i++){
+	move(listaPontos[i].i, listaPontos[i].j);
+	echochar(' ');
   }
+  listaPontos.clear();
 
   // Desenha corpos na tela
   std::vector<Corpo *> *corpos = this->lista->get_corpos();
 
   for (int k=0; k<corpos->size(); k++)
   {
-    i = (int) ((*corpos)[k]->get_posicao()) * \
-        (this->maxI / this->maxX);
-    move(i, k);   /* Move cursor to position */
-    echochar('*');  /* Prints character, advances a position */
+    i = (int) ((*corpos)[k]->get_posicao() * (this->maxI - 1) / this->maxX);
+	if (i >= 0 && i < maxI){
+		move(i, k);   /* Move cursor to position */
+		echochar('*');  /* Prints character, advances a position */
+		listaPontos.push_back((ponto){i,k});  /*Marcar posição do ponto desenhado */
 
-    // Atualiza corpos antigos
-    (*corpos_old)[k]->update(  (*corpos)[k]->get_velocidade(),\
-                               (*corpos)[k]->get_posicao());
+	}
   }
 
   // Atualiza tela
